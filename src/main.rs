7325b2ac3315333271ee904c5c21636a0749bdc8
akora <AKORA>
2022-10-27 22:02:04 +0200

    implement mandelbrot

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use num_complex::Complex;
 use tiny_skia::*;
 
-// Based on https://fiddle.skia.org/c/@compose_path
+const X_RANGE: (f64, f64) = (-2.00, 0.47);
+const Y_RANGE: (f64, f64) = (-1.12, 1.12);
+
+const IMAGE_SIZE: (u32, u32) = (500, 500);
+const X_SCALE: f64 = (IMAGE_SIZE.0 as f64) / (-(X_RANGE.0 - X_RANGE.1));
+const Y_SCALE: f64 = (IMAGE_SIZE.1 as f64) / (-(Y_RANGE.0 - Y_RANGE.1));
+
+const MAX_ITERATION: u64 = 1000;
+const RADIUS: f64 = 2.;
 
 fn main() {
     let mut paint = Paint::default();
@@ -24,7 +33,32 @@ fn main() {
     stroke.line_cap = LineCap::Round;
     stroke.dash = StrokeDash::new(vec![20.0, 40.0], 0.0);
 
-    let mut pixmap = Pixmap::new(500, 500).unwrap();
+    let mut pixmap = Pixmap::new(IMAGE_SIZE.0, IMAGE_SIZE.1).unwrap();
     pixmap.stroke_path(&path, &paint, &stroke, Transform::identity(), None);
     pixmap.save_png("image.png").unwrap();
+}
+
+fn mandelbrot(px: f64, py: f64) -> u64 {
+    let x0 = px - (IMAGE_SIZE.0 / 2) as f64;
+    let y0 = py - (IMAGE_SIZE.1 / 2) as f64;
+
+    let x0 = x0 / X_SCALE;
+    let y0 = y0 / Y_SCALE;
+
+    let mut x = 0.0;
+    let mut y = 0.0;
+    let mut iteration = 0_u64;
+
+    //println!("{x} : {y}");
+    while ((x * x + y * y) <= RADIUS * RADIUS) && (iteration < MAX_ITERATION) {
+        let xtemp = x * x - y * y + x0;
+        y = 2. * x * y + y0;
+        x = xtemp;
+        iteration += 1;
+    }
+
+    //color := palette[iteration]
+    //plot(Px, Py, color)
+
+    iteration
 }
