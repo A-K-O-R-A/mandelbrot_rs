30ecf18a8449c231b9358f1aacd56f2e654bbafa
akora <AKORA>
2022-10-27 22:29:28 +0200

    center mandelbrot

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use num_complex::Complex;
 use tiny_skia::*;
 
 const X_RANGE: (f64, f64) = (-2.00, 0.47);
+const X_OFF: f64 = (X_RANGE.0 + X_RANGE.1) / 2.;
 const Y_RANGE: (f64, f64) = (-1.12, 1.12);
+const Y_OFF: f64 = (Y_RANGE.0 + Y_RANGE.1) / 2.;
 
 const IMAGE_SIZE: (u32, u32) = (500, 500);
 const X_SCALE: f64 = (IMAGE_SIZE.0 as f64) / (-(X_RANGE.0 - X_RANGE.1));
@@ -13,37 +15,43 @@ const RADIUS: f64 = 2.;
 
 fn main() {
     let mut paint = Paint::default();
-    paint.set_color_rgba8(0, 127, 0, 200);
-    paint.anti_alias = true;
-
-    let path = {
-        let mut pb = PathBuilder::new();
-        const RADIUS: f32 = 250.0;
-        const CENTER: f32 = 250.0;
-        pb.move_to(CENTER + RADIUS, CENTER);
-        for i in 1..8 {
-            let a = 2.6927937 * i as f32;
-            pb.line_to(CENTER + RADIUS * a.cos(), CENTER + RADIUS * a.sin());
-        }
-        pb.finish().unwrap()
-    };
-
-    let mut stroke = Stroke::default();
-    stroke.width = 6.0;
-    stroke.line_cap = LineCap::Round;
-    stroke.dash = StrokeDash::new(vec![20.0, 40.0], 0.0);
+    //paint.set_color_rgba8(0, 127, 0, 200);
+    //paint.anti_alias = true;
 
     let mut pixmap = Pixmap::new(IMAGE_SIZE.0, IMAGE_SIZE.1).unwrap();
-    pixmap.stroke_path(&path, &paint, &stroke, Transform::identity(), None);
+
+    let mut x = 0.;
+    while (x as u32) < IMAGE_SIZE.0 {
+        let mut y = 0.;
+        while (y as u32) < IMAGE_SIZE.1 {
+            let rect = Rect::from_xywh(x, y, 1., 1.).expect("Couldn't create rect");
+
+            let iter = mandelbrot(x as f64, y as f64);
+            paint.shader = Shader::SolidColor(iteration_to_color(iter));
+            //paint.set_color_rgba8(100, 0, ((iter / MAX_ITERATION) * 255) as u8, 255);
+            pixmap.fill_rect(rect, &paint, Transform::identity(), None);
+            y += 1.;
+            //println!("{x} : {y}");
+        }
+        x += 1.;
+        //println!("{x} : {y}");
+    }
+
     pixmap.save_png("image.png").unwrap();
 }
 
+fn iteration_to_color(iteration: u64) -> Color {
+    let iter_fact = iteration as f32 / MAX_ITERATION as f32;
+    Color::from_rgba8(0, 0, (iter_fact * 255.) as u8, 255)
+}
+
+///Source: https://en.wikipedia.org/wiki/Mandelbrot_set
 fn mandelbrot(px: f64, py: f64) -> u64 {
     let x0 = px - (IMAGE_SIZE.0 / 2) as f64;
     let y0 = py - (IMAGE_SIZE.1 / 2) as f64;
 
-    let x0 = x0 / X_SCALE;
-    let y0 = y0 / Y_SCALE;
+    let x0 = (x0 / X_SCALE) + X_OFF;
+    let y0 = (y0 / Y_SCALE) + Y_OFF;
 
     let mut x = 0.0;
     let mut y = 0.0;
