f0de07d97e42dc62e66ea10d189f5c966dc7f6aa
akora <AKORA>
2022-11-02 13:48:09 +0100

    formatting

--- src/main.rs
+++ src/main.rs
@@ -7,37 +7,15 @@ mod color;
 mod data;
 mod sets;
 
-pub const IMAGE_SIZE: (usize, usize) = (2000, 1000);
+pub const IMAGE_SIZE: (usize, usize) = (4000, 2000);
 pub const MAX_ITERATION: u64 = 1_000;
 
 pub type Color = [u8; 3];
 
 fn main() {
-    let pb = Arc::new(Mutex::new(ProgressBar::new((IMAGE_SIZE.1) as u64)));
     let now = Instant::now();
 
-    let y_range = 0..IMAGE_SIZE.1;
-    let yx_map: Vec<Vec<Color>> = y_range
-        .into_par_iter()
-        .map(move |y| {
-            let x_range = 0..IMAGE_SIZE.0;
-
-            let colors = x_range
-                .into_par_iter()
-                .map(|x| {
-                    //Get iteration count
-                    let iter = sets::mandelbrot::get_pixel(x as f64, y as f64);
-
-                    color::from_iterations(iter)
-                })
-                .collect();
-
-            //Reduces speed a bit
-            pb.lock().unwrap().inc();
-
-            colors
-        })
-        .collect();
+    let yx_map = collect_color_map();
 
     let elapsed = now.elapsed();
     println!("Calculation took      {:.2?}", elapsed);
@@ -57,4 +35,32 @@ fn main() {
 
     let elapsed = now.elapsed();
     println!("Encoding/Writing      {:.2?}", elapsed);
+}
+
+#[allow(dead_code)]
+fn collect_color_map() -> Vec<Vec<Color>> {
+    let pb = Arc::new(Mutex::new(ProgressBar::new((IMAGE_SIZE.1) as u64)));
+    let y_range = 0..IMAGE_SIZE.1;
+
+    y_range
+        .into_par_iter()
+        .map(move |y| {
+            let x_range = 0..IMAGE_SIZE.0;
+
+            let colors = x_range
+                .into_par_iter()
+                .map(|x| {
+                    //Get iteration count
+                    let iter = sets::mandelbrot::get_pixel(x as f64, y as f64);
+
+                    color::from_iterations(iter)
+                })
+                .collect();
+
+            //Reduces speed a bit
+            pb.lock().unwrap().inc();
+
+            colors
+        })
+        .collect()
 }
